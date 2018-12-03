913c729be81dde043882ff4710daf8309afef3e8
Jeremie Demack <jdemack@gmail.com>
2018-12-03 14:14:31 -0500

    day 1 labs 1-7

---
+++ lab_input/input_ip.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Written By: Jeremie Demack
+# Collect IP address information from a user, and display it back to them.
+
+## Collect IP address from the user
+user_input = input('Please enter an IPv4 IP address:')
+
+## Collect vendor name from the user
+vendor_name = input('Please enter the vendor name:')
+
+## Print out the IP address collected from the user
+print("You told me to IPv4 address is:" + user_input)
+
+## Print out the vendor name collected from the user
+print("You told me the vendor name is:" + vendor_name)
