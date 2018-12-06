34806485085ae999415236f0082c8237856eecdd
Jeremie Demack <jdemack@gmail.com>
2018-12-06 13:20:46 -0500

    Lab 30

--- netfunct01/custom01.py
+++ netfunct01/custom01.py
@@ -8,8 +8,14 @@ def commandpush(devicecmd): # devicecmd==list
         print('REBOOTING NOW! --> ')
             # we'll learn to write code that sends cmds to device here
 
+# funtion to iterate through a list of IPs
+def devicereboot(iplist):
+    for z in iplist:
+        print('Connecting to...' + z + '.' + ' REBOOTING NOW!')
+
 ### Start our script
 work2do = ["10.1.0.1", "10.2.0.1", "10.3.0.1"] # data that replaces data stored in file
+ListOfIPs = ["0.0.0.0", "1.1.1.1", "2.2.2.2", "3.3.3.3"]
 
 print("Welcome to the network device command pusher") # welcome message
 
@@ -18,3 +24,4 @@ print("\nData set found\n") # replace with function call that reads in data from
 
 ## run 
 commandpush(work2do) # call function to push commands to devices
+devicereboot(ListOfIPs)
